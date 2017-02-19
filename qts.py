1526e6e556f568d9b8b4bc240ff60065de87cb46
sagar patwal <sagarpatwal21@gmail.com>
2017-02-19 12:22:21 +0530

    Update qts.py

--- qts.py
+++ qts.py
@@ -57,18 +57,5 @@ class searching_for_quote:
 
 	
 
-'''def main():
-	args1=sys.argv[1:]
-	s_obj=searching_for_quote(args1)	
-	src=raw_input('Type searchterm... ')
-	result=s_obj.getdialouge(src)
-	for i in result:
-		print(i)
-
- 
-if __name__ == '__main__':
-	main()
-	'''
-
 
 
